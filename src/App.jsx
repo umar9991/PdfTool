05b87979bcbbb41efc86b77bce2304f43b32a7d3
umarahmad9991 <umarahmad9u8@gmail.com>
2025-09-09 23:40:37 +0400

    Day 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import './App.css'
 import Navbar from './Home/pages/Navbar'
-import PDFUtils from './Home/pdf/pdfUtils'
-import HeroSection from './Home/heroSection/HeroSection'
 import PDFToolsApp from './Home/pdfToolsApp'
 import Footer from './Home/pages/Footer'
-import Content from './Home/pages/content'
 import PDFToolsLanding from './Home/pages/PDFToolsLanding'
 
 function App() {
@@ -13,17 +10,10 @@ function App() {
   return (
     <>
      <Navbar/>
-    {/* <Content/> */}
-    {/* <HeroSection/> */}
     <PDFToolsApp/>
     <PDFToolsLanding/>
      <Footer/>
-     {/*
-    <HeroSection/>
-  
-    <PDFUtils/> */}
-    {/* <PDFToolsApp/> */}
-    {/* <ToolCard tool={toolData} onClick={handleClick} /> */}
+
 
     </>
   )
